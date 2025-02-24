1efbfed15b4ede37a79deda0d74f5ac4d88fde05
Erick Ayala <erikjesu403@gmail.com>
2025-02-24 12:38:33 -0600

    Mejorar la lógica de movimientos y actualizar las vistas de ubicaciones para incluir áreas y detalles adicionales

--- movimientos.py
+++ movimientos.py
@@ -16,10 +16,10 @@ class Movimientos(models.Model):
     fecha_cambio = fields.Date(string='Fecha de cambio de ubicación', required=True, default=fields.Date.context_today, readonly=True)
     origen_id = fields.Char(
         string='Departamen de Origen',
-        compute='_compute_origen_id',  # Campo computado 
+        compute='_compute_origen_id',  # Campo computado
     )
     destino_id = fields.Char(
-        string='Departamento de Destino', 
+        string='Departamento de Destino',
         compute='_compute_destino_id',  # Campo computado
     )
     tipo_movimiento = fields.Selection([
@@ -27,6 +27,7 @@ class Movimientos(models.Model):
         ('reasignacion', 'Reasignación'),
     ], string='Tipo de movimiento', required=True, default='reasignacion')
     observaciones = fields.Text(string='Observaciones')
+
     detalle_ids = fields.One2many('movimientos.detalle', 'movimiento_id', string='Detalles del movimiento')
 
     @api.depends('detalle_ids.departamento_origen')
@@ -47,7 +48,7 @@ class Movimientos(models.Model):
                 # Obtenemos el nombre del departamento de destino
                 movimiento.destino_id = movimiento.detalle_ids[0].departamento_destino_id.departamento_id.name
             else:
-                movimiento.destino_id = ''             
+                movimiento.destino_id = ''
 
     @api.multi
     def action_aplicar(self):
@@ -56,7 +57,7 @@ class Movimientos(models.Model):
 
         # Cambiar el estado de la entrega a 'aplicado'
         self.write({'status': 'aplicado'})
-    
+
     @api.multi
     def action_cancelar(self):
         # Cambiar el estado de la entrega a 'cancelado'
@@ -67,34 +68,39 @@ class MovimientosDetalle(models.Model):
 
     movimiento_id = fields.Many2one('movimientos.ubicaciones', string='Movimiento')
     producto_id = fields.Many2one('itsa.materiales.productos', string='Producto', required=True, domain=[('tipo_producto', '=', 'activo_fijo')])
-    departamento_origen = fields.Many2one('itsa.materiales.ubicaciones', string='Departamento origen')
-    edificio_origen = fields.Char(string='Edificio origen', compute='_compute_edificioyarea_origen')
-    area_origen = fields.Char(string='Área origen', compute='_compute_edificioyarea_origen')
-    departamento_destino_id = fields.Many2one('itsa.materiales.ubicaciones', string='Departamento destino')
-    edificio_destino = fields.Char(string='Edificio destino', compute='_compute_edificioyarea_destino')
-    area_destino = fields.Char(string='Área destino', compute='_compute_edificioyarea_destino')
+    departamento_origen = fields.Many2one('itsa.materiales.ubicaciones', string='Departamento origen', required=True)
+    edificio_origen = fields.Char(string='Edificio origen', compute='_compute_edificio_origen')
+    area_origen_id = fields.Many2one(
+        'ubicacion.detalle',
+        string='Área de Origen', required=True, 
+        domain="[('ubicacion_id', '=', departamento_origen)]"
+    )
+    departamento_destino_id = fields.Many2one('itsa.materiales.ubicaciones', string='Departamento destino', required=True) 
+
+    edificio_destino = fields.Char(string='Edificio destino', compute='_compute_edificio_destino')
+    area_destino_id = fields.Many2one(
+        'ubicacion.detalle',
+        string='Área de Destino', required=True, 
+        domain="[('ubicacion_id', '=', departamento_destino_id)]"
+    )
     descripcion = fields.Text(string='Descripción')
 
     @api.depends('departamento_origen')
-    def _compute_edificioyarea_origen(self):
-        """Calcula el edificio y el área basados en el departamento seleccionado."""
+    def _compute_edificio_origen(self):
+        """Calcula el edificio basadose en el departamento seleccionado."""
         for record in self:
             if record.departamento_origen:
-                # Asignar el nombre del edificio y el área basados en el departamento
+                # Asignar el nombre del edificio 
                 record.edificio_origen = record.departamento_origen.edificio_id.name
-                record.area_origen = record.departamento_origen.area.name
             else:
                 record.edificio_origen = ''
-                record.area_origen = ''
 
     @api.depends('departamento_destino_id')
-    def _compute_edificioyarea_destino(self):
-        """Calcula el edificio y el área basados en el departamento seleccionado."""
+    def _compute_edificio_destino(self):
+        """Calcula el edificio basadose en el departamento seleccionado."""
         for record in self:
             if record.departamento_destino_id:
-                # Asignar el nombre del edificio y el área basados en el departamento
+                # Asignar el nombre del edificio
                 record.edificio_destino = record.departamento_destino_id.edificio_id.name
-                record.area_destino = record.departamento_destino_id.area.name
             else:
                 record.edificio_destino = ''
-                record.area_destino = ''
--- ubicaciones.py
+++ ubicaciones.py
@@ -1,14 +1,5 @@
 # -*- coding: utf-8 -*-
-from openerp import models, fields, api
-
-
-class Areas(models.Model):
-    _name = 'itsa.materiales.ubicaciones.areas'
-    _rec_name = 'name' # Permite mostrar sus campos como tipo char, para mostrarse desde otro modelo.
-
-    name = fields.Char(string='Area Nueva:', required=True)
-    
-    _sql_constraints = [('itsa_materiales_ubicaciones_areas', 'unique(name)', 'Esta area ya existe')]
+from openerp import models, fields
 
 class ubicaciones(models.Model):
 
@@ -17,5 +8,14 @@ class ubicaciones(models.Model):
 
     name = fields.Char(string='Clave', size=10, required=True)
     departamento_id = fields.Many2one('itsa.base.deptos', string='Departamento', required=True)
-    area = fields.Many2one('itsa.materiales.ubicaciones.areas', string='Area', required=True)  
+    # area = fields.Many2one('itsa.materiales.ubicaciones.areas', string='Area', required=True)  
     edificio_id = fields.Many2one('itsa.base.edificios', string='Edificio', required=True)
+    
+    detalle_ids = fields.One2many('ubicacion.detalle', 'ubicacion_id', string='Detalles de la ubicacion')
+
+class UbicacionDetalle(models.Model):
+    _name = 'ubicacion.detalle'
+
+    name = fields.Char(string='Nombre del Area:', required=True)
+    ubicacion_id = fields.Many2one('itsa.materiales.ubicaciones', string='Departamento', required=True)
+    
